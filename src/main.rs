d9bb8e16aa3d50f56bd38ced0f0ab70ca83abe7f
Matt Paras <matthewparas2020@u.northwestern.edu>
2025-07-14 22:05:45 -0700

    first pass

---
+++ src/main.rs
@@ -0,0 +1,378 @@
+use std::{
+    any::Any,
+    io::Cursor,
+    sync::{Arc, Mutex},
+};
+
+use arrow::ipc::{reader::FileReader, writer::FileWriter};
+use arrow_array::{ArrayRef, UInt64Array};
+use arrow_schema::DataType;
+use async_trait::async_trait;
+use datafusion::{
+    arrow::{
+        array::RecordBatch,
+        datatypes::{Field, Schema, SchemaRef},
+    },
+    catalog::{Session, TableProvider},
+    common::project_schema,
+    datasource::TableType,
+    error::DataFusionError,
+    execution::{SendableRecordBatchStream, TaskContext},
+    logical_expr::dml::InsertOp,
+    physical_plan::{
+        DisplayAs, DisplayFormatType, ExecutionPlan, PlanProperties, execute_input_stream,
+        memory::MemoryStream, stream::RecordBatchStreamAdapter,
+    },
+    prelude::Expr,
+};
+use futures::StreamExt;
+use sled::Db;
+
+#[derive(Debug, Clone)]
+struct SledDb {
+    db: Arc<Mutex<Db<1024>>>,
+    schema: SchemaRef,
+}
+
+#[async_trait]
+impl TableProvider for SledDb {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn schema(&self) -> SchemaRef {
+        self.schema.clone()
+    }
+
+    fn table_type(&self) -> TableType {
+        TableType::Base
+    }
+
+    async fn scan(
+        &self,
+        _state: &dyn Session,
+        projection: Option<&Vec<usize>>,
+        _filters: &[Expr],
+        _limit: Option<usize>,
+    ) -> Result<Arc<dyn ExecutionPlan>, DataFusionError> {
+        return self.create_physical_plan(projection, self.schema()).await;
+    }
+
+    async fn insert_into(
+        &self,
+        _state: &dyn Session,
+        input: Arc<dyn ExecutionPlan>,
+        insert_op: InsertOp,
+    ) -> Result<Arc<dyn ExecutionPlan>, DataFusionError> {
+        self.create_physical_plan_insert(self.schema(), input, insert_op)
+            .await
+    }
+}
+
+impl CustomExec {
+    fn new(projections: Option<&Vec<usize>>, schema: SchemaRef, db: SledDb) -> Self {
+        let projected_schema = project_schema(&schema, projections).unwrap();
+
+        let properties = {
+            let eq_properties = datafusion::physical_expr::EquivalenceProperties::new(schema);
+            let partitioning = datafusion::physical_plan::Partitioning::UnknownPartitioning(1);
+            let emission_type =
+                datafusion::physical_plan::execution_plan::EmissionType::Incremental;
+            let boundedness = datafusion::physical_plan::execution_plan::Boundedness::Bounded;
+            datafusion::physical_plan::PlanProperties::new(
+                eq_properties,
+                partitioning,
+                emission_type,
+                boundedness,
+            )
+        };
+
+        Self {
+            db,
+            projected_schema,
+            properties,
+        }
+    }
+}
+
+impl SledDb {
+    pub(crate) async fn create_physical_plan(
+        &self,
+        projections: Option<&Vec<usize>>,
+        schema: SchemaRef,
+    ) -> Result<Arc<dyn ExecutionPlan>, DataFusionError> {
+        Ok(Arc::new(CustomExec::new(projections, schema, self.clone())))
+    }
+
+    pub(crate) async fn create_physical_plan_insert(
+        &self,
+        schema: SchemaRef,
+        input: Arc<dyn ExecutionPlan>,
+        insert: InsertOp,
+    ) -> Result<Arc<dyn ExecutionPlan>, DataFusionError> {
+        Ok(Arc::new(CustomInsertIntoExec::new(
+            schema,
+            self.clone(),
+            input,
+            insert,
+        )))
+    }
+}
+
+#[derive(Debug)]
+struct CustomInsertIntoExec {
+    db: SledDb,
+    schema: SchemaRef,
+    properties: PlanProperties,
+    insert_op: InsertOp,
+    input: Arc<dyn ExecutionPlan>,
+    count_schema: SchemaRef,
+}
+
+impl CustomInsertIntoExec {
+    fn new(
+        schema: SchemaRef,
+        db: SledDb,
+        input: Arc<dyn ExecutionPlan>,
+        insert_op: InsertOp,
+    ) -> Self {
+        let properties = {
+            let eq_properties =
+                datafusion::physical_expr::EquivalenceProperties::new(schema.clone());
+            let partitioning = datafusion::physical_plan::Partitioning::UnknownPartitioning(1);
+            let emission_type =
+                datafusion::physical_plan::execution_plan::EmissionType::Incremental;
+            let boundedness = datafusion::physical_plan::execution_plan::Boundedness::Bounded;
+            datafusion::physical_plan::PlanProperties::new(
+                eq_properties,
+                partitioning,
+                emission_type,
+                boundedness,
+            )
+        };
+
+        Self {
+            db,
+            schema,
+            properties,
+            insert_op,
+            input,
+            count_schema: make_count_schema(),
+        }
+    }
+}
+
+impl DisplayAs for CustomInsertIntoExec {
+    fn fmt_as(&self, _t: DisplayFormatType, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "CustomInsertIntoExec")
+    }
+}
+
+impl ExecutionPlan for CustomInsertIntoExec {
+    fn name(&self) -> &str {
+        "CustomExec"
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn schema(&self) -> SchemaRef {
+        self.schema.clone()
+    }
+
+    fn properties(&self) -> &PlanProperties {
+        &self.properties
+    }
+
+    fn children(&self) -> Vec<&Arc<dyn ExecutionPlan>> {
+        Vec::new()
+    }
+
+    fn with_new_children(
+        self: Arc<Self>,
+        _: Vec<Arc<dyn ExecutionPlan>>,
+    ) -> Result<Arc<dyn ExecutionPlan>, DataFusionError> {
+        Ok(self)
+    }
+
+    fn execute(
+        &self,
+        partition: usize,
+        context: Arc<TaskContext>,
+    ) -> Result<SendableRecordBatchStream, DataFusionError> {
+        let mut data =
+            execute_input_stream(self.input.clone(), self.schema.clone(), partition, context)?;
+
+        let count_schema = Arc::clone(&self.count_schema);
+
+        let db = self.db.clone();
+        let schema = self.schema.clone();
+
+        let stream = futures::stream::once(async move {
+            let mut buffer = Vec::new();
+
+            let mut count = 0;
+
+            while let Some(batch) = data.next().await {
+                let batch = batch?;
+
+                let guard = db.db.lock().unwrap();
+                let mut writer = FileWriter::try_new(&mut buffer, &schema)?;
+
+                writer.write(&batch)?;
+                writer.finish()?;
+
+                guard.insert("test", buffer.as_slice()).unwrap();
+
+                count += batch.num_rows();
+
+                buffer.clear();
+            }
+
+            Ok(make_count_batch(count as _))
+        })
+        .boxed();
+
+        Ok(Box::pin(RecordBatchStreamAdapter::new(
+            count_schema,
+            stream,
+        )))
+    }
+}
+
+fn make_count_batch(count: u64) -> RecordBatch {
+    let array = Arc::new(UInt64Array::from(vec![count])) as ArrayRef;
+
+    RecordBatch::try_from_iter_with_nullable(vec![("count", array, false)]).unwrap()
+}
+
+fn make_count_schema() -> SchemaRef {
+    // Define a schema.
+    Arc::new(Schema::new(vec![Field::new(
+        "count",
+        DataType::UInt64,
+        false,
+    )]))
+}
+
+#[derive(Debug)]
+struct CustomExec {
+    db: SledDb,
+    projected_schema: SchemaRef,
+    properties: PlanProperties,
+}
+
+impl DisplayAs for CustomExec {
+    fn fmt_as(&self, _t: DisplayFormatType, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "CustomExec")
+    }
+}
+
+impl ExecutionPlan for CustomExec {
+    fn name(&self) -> &str {
+        "CustomExec"
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn schema(&self) -> SchemaRef {
+        self.projected_schema.clone()
+    }
+
+    fn properties(&self) -> &PlanProperties {
+        &self.properties
+    }
+
+    fn children(&self) -> Vec<&Arc<dyn ExecutionPlan>> {
+        Vec::new()
+    }
+
+    fn with_new_children(
+        self: Arc<Self>,
+        _: Vec<Arc<dyn ExecutionPlan>>,
+    ) -> Result<Arc<dyn ExecutionPlan>, DataFusionError> {
+        Ok(self)
+    }
+
+    fn execute(
+        &self,
+        _partition: usize,
+        _context: Arc<TaskContext>,
+    ) -> Result<SendableRecordBatchStream, DataFusionError> {
+        let guard = self.db.db.lock().unwrap();
+
+        let values = guard.iter();
+
+        let mut record_batches = Vec::new();
+        let mut num_rows = 0;
+
+        for value in values.values() {
+            let value = value.unwrap();
+            let slice: &[u8] = &value;
+
+            let cursor = Cursor::new(slice);
+
+            let reader = FileReader::try_new(cursor, None).unwrap();
+            for batch in reader {
+                let batch = batch.unwrap();
+                num_rows += batch.num_rows();
+                record_batches.push(batch);
+            }
+        }
+
+        eprintln!("Read: {}", num_rows);
+
+        Ok(Box::pin(MemoryStream::try_new(
+            record_batches,
+            self.schema(),
+            None,
+        )?))
+    }
+}
+
+use datafusion::execution::context::SessionContext;
+
+#[tokio::main]
+async fn main() {
+    let db: Db<1024> = Db::open_with_config(&sled::Config::new().path("test.db")).unwrap();
+    let schema = Arc::new(Schema::new(vec![Field::new(
+        "foo",
+        datafusion::arrow::datatypes::DataType::Int64,
+        false,
+    )]));
+
+    let sled = SledDb {
+        db: Arc::new(Mutex::new(db)),
+        schema,
+    };
+
+    let ctx = SessionContext::new();
+
+    ctx.register_table("foo", Arc::new(sled)).unwrap();
+
+    let df = ctx
+        .sql(
+            r#"
+INSERT INTO foo
+VALUES
+(10),
+(20),
+(30),
+(40),
+(50);
+"#,
+        )
+        .await
+        .unwrap();
+
+    df.show().await.unwrap();
+
+    let df = ctx.sql("SELECT * FROM foo").await.unwrap();
+
+    df.show().await.unwrap();
+
+    // Ok(())
+}
